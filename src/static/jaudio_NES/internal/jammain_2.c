0983e27bbe166860a49b7984aa89aba1af346325
judexopatatasxd <133681884+judexopatatasxd@users.noreply.github.com>
2026-07-13 15:04:06 -0500

    Remove PC register specifiers from JAudio

--- src/static/jaudio_NES/internal/jammain_2.c
+++ src/static/jaudio_NES/internal/jammain_2.c
@@ -1392,7 +1392,17 @@ void Jam_UpdateTrackAll(seqp_* track)
  * Address:	80010E00
  * Size:	00000C
  */
-static void OSf32tos8(register f32* in, volatile register s8* out)
+static void OSf32tos8(
+#ifndef TARGET_PC
+    register
+#endif
+    f32* in,
+#ifndef TARGET_PC
+    volatile register
+#else
+    volatile
+#endif
+    s8* out)
 {
     *out = __OSf32tos8(*in);
 }
